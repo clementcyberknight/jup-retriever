6560e06f46c39b1ed2f51ea6d9d1f309daf3b5c2
clement <clementakhimien25@gmail.com>
2025-11-11 23:42:36 +0100

    pump-4.0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const tokenRoutes = require('./routes/tokenRoutes');
-const errorHandler = require('./utils/errorHandler');
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const tokenRoutes = require("./routes/tokenRoutes");
+const errorHandler = require("./utils/errorHandler");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const limiter = rateLimit({
+  windowMs: 1000, // 1 second
+  max: 1, // limit each IP to 1 requests per windowMs
+  message: "Too many requests from this IP, please try again after a second",
+});
+
 app.use(express.json());
-app.use('/api/tokens', tokenRoutes);
+app.use("/api/tokens", limiter, tokenRoutes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
